40b1f6b2f789214780f442ff8e372043c370d4be
Wally Jones <wally@wallyjones.com>
2019-03-15 22:51:11 -0400

    A wee bit of clean up

--- gol.py
+++ gol.py
@@ -4,34 +4,31 @@ import numpy as np
 import time
 import os
 
+
 def num_neighbors(grid, pos):
     neighbors = 0
     x,y = pos
 
-    neighbors += grid[x-1][y-1]
-    neighbors += grid[x-1][y]
+    neighbors += grid[x-1][y-1] # Top Left
+    neighbors += grid[x-1][y]   # Top
+    neighbors += grid[x-1][y+1] # Top Right
 
-    if y < len(grid)-1:
-        neighbors += grid[x-1][y+1]
-        neighbors += grid[x][y+1]
+    neighbors += grid[x][y-1]   # Left
+    neighbors += grid[x][y+1]   # Right
 
-    neighbors += grid[x][y-1]
-
-    if x < len(grid[0])-1:
-        neighbors += grid[x+1][y-1]
-        neighbors += grid[x+1][y]
-
-    if x < len(grid[0])-1 and y < len(grid)-1:
-        neighbors += grid[x+1][y+1]
+    neighbors += grid[x+1][y-1] # Bottom Left
+    neighbors += grid[x+1][y]   # Bottom
+    neighbors += grid[x+1][y+1] # Bottom Right
                 
     return neighbors
 
 
 def update_state(grid):
-    h, w = len(grid), len(grid[0])
-    new_grid = np.zeros(shape=(h,w))
-    for x in range(len(grid)-1):
-        for y in range(len(grid[0])-1):
+    h, w = len(grid)-1, len(grid[0])-1
+    new_grid = np.zeros(shape=(h+1,w+1))
+
+    for x in range(h):
+        for y in range(w):
             n = num_neighbors(grid, [x,y])
             cell = grid[x][y]
             if cell == 0 and n == 3:
@@ -45,19 +42,20 @@ def update_state(grid):
 
     return new_grid
 
+
 def print_grid(grid):
     for row in grid:
         for col in row:
             if col == 1:
-                print("⬛", end='')
+                print("⬛️", end='')
             else:
                 print("  ", end='')
         print()
 
 
 def main():
-    width = 120
-    height = 100
+    width = 80
+    height = 60
     grid = np.random.randint(2, size=(height, width)) 
 
     while True:
